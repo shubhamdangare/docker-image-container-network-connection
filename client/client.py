f6a957ff569b9567a030414ba0339671f635ba9e
shubhamdangare <shubham17495@gmail.com>
2019-06-25 17:58:41 +0530

    added for client side

---
+++ client/client.py
@@ -0,0 +1,22 @@
+import socket                  
+s = socket.socket()            
+host = socket.gethostname()    
+port = 6000                  
+
+s.connect((host, port))
+
+with open('received_file', 'wb') as f:
+    print('file opened')
+    while True:
+        print('receiving data...')
+        data = s.recv(1024)
+        print('data=%s', (data))
+        if not data:
+            break
+        # write data to a file
+        f.write(data)
+
+f.close()
+print('Successfully get the file')
+s.close()
+print('connection closed')
